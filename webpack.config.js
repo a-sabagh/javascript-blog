1239ba81151879ac7a32389b6307b9f449e85cbd
a-sabagh <a.sabagh72@gmail.com>
2023-02-02 12:56:06 +0330

    webpack initialized configuration

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: './src/index.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname,'public')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader,'css-loader']
+			},
+			{
+				test: /\.js$/i,
+				use : {
+					loader: 'babel-loader',
+					options: {
+						exclude: ['node_modules'],
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.(png|jpeg)/,
+				use: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			title: 'Prosenal Blog'
+		})
+	]
+}
